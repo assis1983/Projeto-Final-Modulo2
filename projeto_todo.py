d87b4e469fb2e4973d14f118bfb4bb03632b52e1
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-11-04 18:13:49 -0300

    git ststMerge branch 'main' of https://github.com/assis1983/Projeto-Final-Modulo2

--- projeto_todo.py
+++ projeto_todo.py
@@ -6,7 +6,7 @@ from sys import breakpointhook, float_repr_style
 from time import sleep
 from xmlrpc.client import boolean
 from datetime import datetime
-titulo = 'PROJETO MÓDULO 2'
+titulo = 'PROJETO FINAL MÓDULO 2 CURSO DE PYTHON DEVELOPMENT'
 print(titulo.center (100, '*'))
 
 conexao = sqlite3.connect('bancotodo.sqlite3')
